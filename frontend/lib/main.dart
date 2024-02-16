f2fce592d4fb9813a0fa9c1ade04da88f638ba58
nithiny <Nithin.Y@ibm.com>
2024-02-17 01:13:42 +0530

    feat: Project setup

---
+++ frontend/lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HackTKM());
+}
+
+class HackTKM extends StatelessWidget {
+  const HackTKM({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
