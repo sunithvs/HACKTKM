08520750f60c781bb0421dfb1025ec07f5dc5c45
sunithvs <sunithvs2002@gmail.com>
2024-02-17 14:26:05 +0530

    fix: location data format on pooling post req

--- backend/pooling/serializers.py
+++ backend/pooling/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.gis.geos import Point
 from rest_framework import serializers
 
 from auth_login.models import User
@@ -17,6 +18,8 @@ class PoolingRequestSerializer(serializers.ModelSerializer):
 
     total_amount_received = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
     remaining_amount_to_be_pooled = serializers.DecimalField(max_digits=10, decimal_places=2, read_only=True)
+    location_lat = serializers.FloatField(source='location.y', write_only=True)
+    location_long = serializers.FloatField(source='location.x', write_only=True)
 
     class Meta:
         model = PoolingRequest
@@ -25,10 +28,25 @@ class PoolingRequestSerializer(serializers.ModelSerializer):
                   'image', 'location']
         read_only_fields = ['is_fulfilled', 'total_amount_received', 'remaining_amount_to_be_pooled']
 
+    def validate_location_data(self, lat, long, srid=4326):
+        # Validate and convert the input location to a GEOSGeometry object
+        try:
+            location = Point(long, lat, srid=srid)
+        except (ValueError, TypeError, KeyError):
+            raise serializers.ValidationError("Invalid location format. Please provide 'lat', 'long', and 'srid'.")
+
+        return location
+
     def create(self, validated_data):
         # If the request is made by an authenticated user, use that user
         user = self.context['request'].user if self.context['request'].user.is_authenticated else None
+        location_data_lat = validated_data.pop('location_lat', None)
+        location_data_long = validated_data.pop('location_long', None)
 
+        if location_data_lat and location_data_long:
+            validated_data['location'] = self.validate_location_data(location_data_lat, location_data_long)
+        else:
+            validated_data['location'] = Point(0, 0, srid=4326)
         # If the user is not authenticated and no user is provided, create a new user
         if not user and not validated_data.get('user'):
             user = User.objects.filter().first()
